02f02a246ed737fa7d5103bb62c198e741df1990
vivek carpenter <105868629+Qhfdfj@users.noreply.github.com>
2024-02-27 10:36:13 +0530

    secound commit

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import Signin from './componenets/Signup/Signin/Signin.js'
 import Signup from './componenets/Signup/Signup.js';
 import Details from './componenets/Signup/Details.js';
 import Errror from './componenets/Errrror.js';
-import {Routes,Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-  
-     
-       
-       <Routes>
+
+
+
+    <Routes>
       <Route path='/first_react' element={<Signup />} />
       <Route path='/signin' element={<Signin />} />
       <Route path='/details' element={<Details />} />
       <Route path='*' element={<Errror />} />
     </Routes>
-    
 
-    
-  
+
+
+
   );
 }
 
